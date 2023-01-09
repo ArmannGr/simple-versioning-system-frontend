20ca4b1ec7c90f6dedbbbf142e0a6a08ab149089
Alex8416 <a.eisner.1822@gmail.com>
2023-01-09 19:22:52 +0100

    Added Angular Material Added basic List with options Added Array as dummy Added Buttons for Actions
    To do: optimize the frontend to support all functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatButtonModule,
+      MatCardModule,
+      MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
