1c5c7d05cceb1c09eb8f6c5ad45493eab0072d14
Mauritz Langer <mauritz@langer-net.de>
2023-01-15 17:00:11 +0100

    Add Proxy and add to the api-url a /api

--- src/app/data/service/file.service.ts
+++ src/app/data/service/file.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {TextFile} from "../types/TextFile";
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class FileService {
   constructor(private http: HttpClient) { }
 
   addFile(textFile: TextFile){
-    return(this.http.post('addFile', textFile));
+    return(this.http.post('api/addFile', textFile));
   }
 }
